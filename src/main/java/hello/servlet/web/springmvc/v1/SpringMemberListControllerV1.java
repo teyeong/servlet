ae43797e5fcc1df4389a8f771085bc372effd9bd
teyeong <taetae0202@naver.com>
2024-01-30 00:08:10 +0900

    미사용 import 제거

--- src/main/java/hello/servlet/web/springmvc/v1/SpringMemberListControllerV1.java
+++ src/main/java/hello/servlet/web/springmvc/v1/SpringMemberListControllerV1.java
@@ -2,13 +2,11 @@ package hello.servlet.web.springmvc.v1;
 
 import hello.servlet.domain.member.Member;
 import hello.servlet.domain.member.MemberRepository;
-import hello.servlet.web.frontcontroller.ModelView;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.ModelAndView;
 
 import java.util.List;
-import java.util.Map;
 
 @Controller
 public class SpringMemberListControllerV1 {
